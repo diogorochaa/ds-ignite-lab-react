28392d5fc12ad4a6da9992aaff16043a4cf4ae11
DiogoRocha10 <diogorocha12@live.com>
2022-10-13 19:30:03 -0300

    feat: storybook main

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    '@storybook/addon-a11y'
   ],
   "framework": "@storybook/react",
   "core": {
@@ -16,11 +16,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) =>
-  {
+  viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = 'ds-ignite-lab'
+      config.base = '/ignite-lab-design-system/'
     }
+
     return config
   }
 }
