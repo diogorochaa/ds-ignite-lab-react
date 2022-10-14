00b8a55abb9c8714423701d804371fb6060030dd
DiogoRocha10 <diogorocha12@live.com>
2022-10-13 21:33:57 -0300

    feat: storybook deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ds-ignite-lab'
+      config.base = '/ds-ignite-lab/'
     }
 
     return config
